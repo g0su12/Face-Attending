8bdc691a8314d7ab63efdb66af7b29c867a6691c
Huy Pham <huypham8501@gmail.com>
2023-12-08 22:00:11 +0700

    update

--- src/components/form/FormAddUser.jsx
+++ src/components/form/FormAddUser.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Button,
   Chip,
@@ -10,24 +10,26 @@ import {
   Radio,
   RadioGroup,
   Select,
-  TextField
+  TextField,
 } from '@mui/material';
-import Stack from "@mui/material/Stack";
-import {getDatabase, onValue, ref} from "firebase/database";
-import MenuItem from "@mui/material/MenuItem";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import Box from "@mui/material/Box";
-import {useAuth} from "../../AuthContext";
-import {getAuth} from "firebase/auth";
-import {useFormik} from "formik";
-import {validate} from "../../common/Schema/userSchema";
+import Stack from '@mui/material/Stack';
+import { getDatabase, onValue, ref } from 'firebase/database';
+import MenuItem from '@mui/material/MenuItem';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Box from '@mui/material/Box';
+import { useAuth } from '../../AuthContext';
+import { getAuth } from 'firebase/auth';
+import { useFormik } from 'formik';
+import { validate } from '../../common/Schema/userSchema';
 import {
   insertStudentToCourse,
-  insertStudentToSession, insertTeacherToCourse, insertTeacherToSession,
+  insertStudentToSession,
+  insertTeacherToCourse,
+  insertTeacherToSession,
   writeStudentData,
   writeTeacherData,
-  writeUserData
-} from "../../common/services/services";
+  writeUserData,
+} from '../../common/services/services';
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -40,7 +42,7 @@ const MenuProps = {
   },
 };
 
-function FormAddUser({handleClose}) {
+function FormAddUser({ handleClose }) {
   const dbRef = getDatabase();
   const coursesRef = ref(dbRef, 'Courses');
   const usersRef = ref(dbRef, 'Users');
@@ -73,54 +75,26 @@ function FormAddUser({handleClose}) {
   }, []);
 
   const formik = useFormik({
-    initialValues:
-      {
-        email: '',
-        password: '',
-        confirmPassword: '',
-        id: '',
-        role: 'teacher',
-        dob: '',
-        gender: 'male',
-        mainClass: '',
-        name: '',
-        photo: '',
-        courses: [],
-      },
+    initialValues: {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      id: '',
+      role: 'teacher',
+      dob: '',
+      gender: 'male',
+      mainClass: '',
+      name: '',
+      photo: '',
+      courses: [],
+    },
     validate,
     onSubmit: async (values) => {
       const { confirmPassword, password, role, ...userData } = values;
 
-      // insert student to course students db
-      if (role === "student"){
-        values.courses.map(course => {
-          const courseId = course.split(":")[0];
-          insertStudentToCourse(courseId, values.id, values.photo, values.name);
-          const courseInfo = listCourses.find(course => course.id === courseId);
-          const sessionInfo = courseInfo.sessions;
-          for (const date in sessionInfo) {
-            for(const id in sessionInfo[date]){
-              insertStudentToSession(date, id, values.id);
-            }
-          }
-        })
-      }else {
-        values.courses.map(course => {
-          const courseId = course.split(":")[0];
-          insertTeacherToCourse(courseId, values.id);
-          const courseInfo = listCourses.find(course => course.id === courseId);
-          const sessionInfo = courseInfo.sessions;
-          for (const date in sessionInfo) {
-            for(const id in sessionInfo[date]){
-              insertTeacherToSession(date, id, values.id, values.name);
-            }
-          }
-        })
-      }
-
       userData.courses = userData.courses.reduce((acc, course) => {
         const [courseCode, isSelected] = course.split(': ');
-        acc[courseCode] = isSelected === "true";
+        acc[courseCode] = isSelected === 'true';
         return acc;
       }, {});
       const dobParts = userData.dob.split('-');
@@ -128,163 +102,187 @@ function FormAddUser({handleClose}) {
         userData.dob = `${dobParts[2]}-${dobParts[1]}-${dobParts[0]}`;
       }
 
-      const auth = getAuth();
       const uid = await signup(values.email, values.password);
       // insert user to Users table
-      writeUserData(uid, values.id, values.email, values.role);
-      if (values.role === "student") {
-        writeStudentData(userData);
-      }else {
-        const {dob, mainClass, ...teacherData} = userData;
-        writeTeacherData(teacherData);
+      await writeUserData(uid, values.id, values.email, values.role);
+      if (values.role === 'student') {
+        await writeStudentData(userData);
+        values.courses.map(async (course) => {
+          const courseId = course.split(':')[0];
+          await insertStudentToCourse(courseId, values.id, values.photo, values.name);
+          const courseInfo = listCourses.find((course) => course.id === courseId);
+          const sessionInfo = courseInfo.sessions;
+          for (const date in sessionInfo) {
+            for (const id in sessionInfo[date]) {
+              await insertStudentToSession(date, id, values.id);
+            }
+          }
+        });
+      } else {
+        const { dob, mainClass, ...teacherData } = userData;
+        await writeTeacherData(teacherData);
+        values.courses.map(async (course) => {
+          const courseId = course.split(':')[0];
+          await insertTeacherToCourse(courseId, values.id);
+          const courseInfo = listCourses.find((course) => course.id === courseId);
+          const sessionInfo = courseInfo.sessions;
+          for (const date in sessionInfo) {
+            for (const id in sessionInfo[date]) {
+              await insertTeacherToSession(date, id, values.id, values.name);
+            }
+          }
+        });
       }
       handleClose();
     },
   });
 
-  return (<Container maxWidth="sm">
-    <form onSubmit={formik.handleSubmit}>
-      <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{my: 2}}/>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <TextField
-            id="email"
-            name="email"
-            label="Email"
-            variant="outlined"
-            fullWidth
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            error={formik.touched.email && Boolean(formik.errors.email)}
-            helperText={formik.touched.email && formik.errors.email}
-          />
+  return (
+    <Container maxWidth="sm">
+      <form onSubmit={formik.handleSubmit}>
+        <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{ my: 2 }} />
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              id="email"
+              name="email"
+              label="Email"
+              variant="outlined"
+              fullWidth
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Mật khẩu"
+              name="password"
+              type="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              fullWidth
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Nhập lại mật khẩu"
+              name="confirmPassword"
+              type="password"
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              fullWidth
+              error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
+              helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Tên người dùng"
+              name="name"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              fullWidth
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="ID"
+              name="id"
+              value={formik.values.id}
+              onChange={formik.handleChange}
+              fullWidth
+              error={formik.touched.id && Boolean(formik.errors.id)}
+              helperText={formik.touched.id && formik.errors.id}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel>Vai trò:</FormLabel>
+            <RadioGroup row name="role" value={formik.values.role} onChange={formik.handleChange}>
+              <FormControlLabel value="teacher" control={<Radio />} label="Giảng viên" />
+              <FormControlLabel value="student" control={<Radio />} label="Sinh viên" />
+            </RadioGroup>
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel>Ngày sinh:</FormLabel>
+            <TextField
+              name="dob"
+              type="date"
+              value={formik.values.dob}
+              onChange={formik.handleChange}
+              fullWidth
+              error={formik.touched.dob && Boolean(formik.errors.dob)}
+              helperText={formik.touched.dob && formik.errors.dob}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormLabel>Giới tính:</FormLabel>
+            <RadioGroup
+              row
+              name="gender"
+              value={formik.values.gender}
+              onChange={formik.handleChange}
+            >
+              <FormControlLabel value="male" control={<Radio />} label="Nam" />
+              <FormControlLabel value="female" control={<Radio />} label="Nữ" />
+            </RadioGroup>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Lớp hành chính"
+              name="mainClass"
+              value={formik.values.mainClass}
+              onChange={formik.handleChange}
+              fullWidth
+              error={formik.touched.mainClass && Boolean(formik.errors.mainClass)}
+              helperText={formik.touched.mainClass && formik.errors.mainClass}
+            />
+          </Grid>
+          <Grid item width={300}>
+            <InputLabel variant="standard" htmlFor="uncontrolled-native">
+              Chọn khoá học:
+            </InputLabel>
+            <Select
+              label="Khoá học"
+              name="courses"
+              multiple
+              value={formik.values.courses}
+              onChange={formik.handleChange}
+              input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+              renderValue={(selected) => (
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                  {selected.map((value) => (
+                    <Chip key={value} label={value.split(':')[0]} />
+                  ))}
+                </Box>
+              )}
+              MenuProps={MenuProps}
+            >
+              {Object.entries(listCourses).map(([key, value]) => (
+                <MenuItem key={key} value={`${value.id}: ${true}`}>
+                  {value.id}: {value.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Mật khẩu"
-            name="password"
-            type="password"
-            value={formik.values.password}
-            onChange={formik.handleChange}
-            fullWidth
-            error={formik.touched.password && Boolean(formik.errors.password)}
-            helperText={formik.touched.password && formik.errors.password}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Nhập lại mật khẩu"
-            name="confirmPassword"
-            type="password"
-            value={formik.values.confirmPassword}
-            onChange={formik.handleChange}
-            fullWidth
-            error={formik.touched.confirmPassword && Boolean(formik.errors.confirmPassword)}
-            helperText={formik.touched.confirmPassword && formik.errors.confirmPassword}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Tên người dùng"
-            name="name"
-            value={formik.values.name}
-            onChange={formik.handleChange}
-            fullWidth
-            error={formik.touched.name && Boolean(formik.errors.name)}
-            helperText={formik.touched.name && formik.errors.name}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="ID"
-            name="id"
-            value={formik.values.id}
-            onChange={formik.handleChange}
-            fullWidth
-            error={formik.touched.id && Boolean(formik.errors.id)}
-            helperText={formik.touched.id && formik.errors.id}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormLabel>Vai trò:</FormLabel>
-          <RadioGroup row name="role" value={formik.values.role} onChange={formik.handleChange}>
-            <FormControlLabel value="teacher" control={<Radio/>} label="Giảng viên"/>
-            <FormControlLabel value="student" control={<Radio/>} label="Sinh viên"/>
-          </RadioGroup>
-        </Grid>
-        <Grid item xs={12}>
-          <FormLabel>Ngày sinh:</FormLabel>
-          <TextField
-            name="dob"
-            type="date"
-            value={formik.values.dob}
-            onChange={formik.handleChange}
-            fullWidth
-            error={formik.touched.dob && Boolean(formik.errors.dob)}
-            helperText={formik.touched.dob && formik.errors.dob}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormLabel>Giới tính:</FormLabel>
-          <RadioGroup row name="gender" value={formik.values.gender}
-                      onChange={formik.handleChange}>
-            <FormControlLabel value="male" control={<Radio/>} label="Nam"/>
-            <FormControlLabel value="female" control={<Radio/>} label="Nữ"/>
-          </RadioGroup>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Lớp hành chính"
-            name="mainClass"
-            value={formik.values.mainClass}
-            onChange={formik.handleChange}
-            fullWidth
-            error={formik.touched.mainClass && Boolean(formik.errors.mainClass)}
-            helperText={formik.touched.mainClass && formik.errors.mainClass}
-          />
-        </Grid>
-        <Grid item width={300}>
-          <InputLabel variant="standard" htmlFor="uncontrolled-native">
-            Chọn khoá học:
-          </InputLabel>
-          <Select
-            label="Khoá học"
-            name="courses"
-            multiple
-            value={formik.values.courses}
-            onChange={formik.handleChange}
-            input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-            renderValue={(selected) => (
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                {selected.map((value) => (
-                  <Chip key={value} label={value.split(":")[0]} />
-                ))}
-              </Box>
-            )}
-            MenuProps={MenuProps}
-          >
-            {Object.entries(listCourses).map(([key, value]) => (
-              <MenuItem
-                key={key}
-                value={`${value.id}: ${true}`}
-              >
-                {value.id}: {value.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </Grid>
-      </Grid>
-      <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{my: 1}}/>
-      <Stack direction="row" spacing={2}>
-        <Button type="submit" variant="contained" color="primary">
-          Thêm
-        </Button>
-        <Button variant="contained" color="error" onClick={handleClose}>
-          Huỷ
-        </Button>
-      </Stack>
-    </form>
-  </Container>);
+        <Stack direction="row" alignItems="center" justifyContent="flex-end" sx={{ my: 1 }} />
+        <Stack direction="row" spacing={2}>
+          <Button type="submit" variant="contained" color="primary">
+            Thêm
+          </Button>
+          <Button variant="contained" color="error" onClick={handleClose}>
+            Huỷ
+          </Button>
+        </Stack>
+      </form>
+    </Container>
+  );
 }
 
 export default FormAddUser;
